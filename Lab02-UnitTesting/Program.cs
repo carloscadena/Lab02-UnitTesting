5bb4b48f4656de4e015519d9a24753d9f37104c1
carlos cadena <carloscadena@live.com>
2018-10-10 22:23:17 -0700

    initial user menu prompt to user

--- Lab02-UnitTesting/Program.cs
+++ Lab02-UnitTesting/Program.cs
@@ -4,9 +4,25 @@ namespace Lab02_UnitTesting
 {
     class Program
     {
+        public static int bankBalance = 1000;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            UserMenu();
+            
         }
+
+        static void UserMenu()
+        {
+            Console.WriteLine("Welcome to Cadena Bank. What would you like to do?");
+            Console.WriteLine("1) Check your BALANCE");
+            Console.WriteLine("2) Make a DEPOSIT");
+            Console.WriteLine("3) Make a WITHDRAWAL");
+            Console.WriteLine("4) EXIT");
+            HandleUserChoice();
+        }
+        
+        
     }
 }
---
+++ Lab02Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using Lab02-UnitTesting;
+
+namespace Lab02Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
